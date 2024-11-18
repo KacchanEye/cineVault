96c03da1e3bd49271fde941aefbebc2804ebc99d
Giuseppe <maranog2000@gmail.com>
2024-11-18 01:48:27 +0100

    navbar and searchbar init

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import './NavBar.css'
+
+
+function NavBar(){
+
+    return(
+        <div>
+
+        <nav>
+        <button>Come funziona</button>
+        <button>Contatti</button>
+        
+
+        </nav>
+
+
+        </div>
+    )
+}
+
+
+
+export default NavBar
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import './SearchBar.css'
+
+function SearchBar() {
+  
+    return (
+      <div>
+      
+      <h1>CineVault</h1>
+      <input type='text' name='searchbar' id='searchbar' placeholder=' Inserisci il titolo del film'></input>
+
+      </div>
+
+    )
+  }
+  
+  export default SearchBar
+  
